e0a0cfbcde9a406a8d9afe83ca8f55c082b89423
sai <2495620791@qq.com>
2022-07-11 20:34:24 +0800

    2022/07/11 周一 20:34:24.11

---
+++ auto-push.bat
@@ -0,0 +1,16 @@
+@echo off
+echo "DOCS PUSH BAT"
+
+echo "1. Start submitting code to the local repository"
+git add .
+ 
+echo "2. Commit the changes to the local repository"
+set now=%date% %time%
+echo "Time:" %now%
+git commit -m "%now%"
+ 
+echo "3. Push the changes to the remote git server"
+git push
+ 
+echo "Batch execution complete!"
+pause
